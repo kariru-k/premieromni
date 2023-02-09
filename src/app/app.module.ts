b69260d6e30e93ce74e37b53daf6fad6b1b60672
kariru-k <karirukeith@students.uonbi.ac.ke>
2023-02-09 17:33:15 +0300

    Add banner component with images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
